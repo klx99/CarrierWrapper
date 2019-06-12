31ceab56d0ef63213bf60aec376b2109e9f3f0f6
mengxk <xiaokun.meng@qcast.cn>
2019-06-12 13:58:30 +0800

    add carrier libraries.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,8 @@
 #include <jni.h>
 #include <string>
 
+#include <ela_carrier.h>
+
 extern "C" JNIEXPORT jstring
 
 JNICALL
@@ -8,5 +10,9 @@ Java_org_elastos_carrierwrapper_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+
+
+    ela_log_init(ElaLogLevel_Debug, nullptr, nullptr);
+
     return env->NewStringUTF(hello.c_str());
 }
